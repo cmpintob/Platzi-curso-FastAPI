520c424f633cbeb5870f72dc3eb36e927e9290b0
Cristhian Pinto <cmpintob@gmail.com>
2023-09-14 16:46:46 -0500

    implementamos schemas y usamos el modulo opcional de typing, con ello, tenemos personalizadas las peliculas segun el schema

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 app.title = "Mi aplicacion con FastAPI"
 app.version = "0.0.1"
+
+class Movie(BaseModel):
+    id: Optional[int] = None
+    title: str
+    overview: str
+    year: int
+    rating: float
+    category: str
 
 movies = [
     {
@@ -51,26 +61,19 @@ def get_movies_by_category(category: str, year: int):
     return [item for item in movies if item["category"] == category]
 
 @app.post('/movies', tags=['movies'])
-def create_movie(id: int = Body(), tittle: str = Body(), overview: str = Body(), year: str = Body(), rating: float = Body(), category: str = Body()):
-    movies.append({
-        "id": id,
-		"title": tittle,
-		"overview": overview,
-		"year": year,
-		"rating": rating,
-		"category": category        
-    })
+def create_movie(movie: Movie):
+    movies.append(movie)
     return movies
 
 @app.put('/movies/{id}', tags=['movies'])
-def modify_movie(id: int, tittle: str = Body(), overview: str = Body(), year: str = Body(), rating: float = Body(), category: str = Body()):
+def modify_movie(id: int, movie: Movie):
     for item in movies:
         if item["id"] == id:
-            item["title"] = tittle
-            item["overview"] = overview
-            item["year"] = year
-            item["rating"] = rating
-            item["category"] = category
+            item["title"] = movie.title
+            item["overview"] = movie.overview
+            item["year"] = movie.year
+            item["rating"] = movie.rating
+            item["category"] = movie.category
         return movies
     return "No existe el id indicado"
 
